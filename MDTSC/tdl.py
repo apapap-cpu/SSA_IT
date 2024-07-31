fd53f59ff6d6ba07a59a640fdcdb77813f259dc5
apapap-cpu <apapapppa@163.com>
2024-07-31 10:08:22 +0800

    Update tdl.py

--- MDTSC/tdl.py
+++ MDTSC/tdl.py
@@ -1,20 +1,18 @@
 # tensor base learning
-# 基于张量处理优化频域内数据
 import scipy.optimize as sco
 import numpy as np
 from parm import Para as P
-# FFT:快速傅里叶变换 IFFT:反快速傅里叶变换
 
-def tendl(Xhat,S,P): # 输入数据的FFT变换结果，另一组数据，参数实例
+def tendl(Xhat,S,P):
     r = P.r
     Shat = np.fft.fft(S,axis = -1)
-    dual_lambda = 10*abs(np.random.randn(r)) # 优化过程中的对偶变量
+    dual_lambda = 10*abs(np.random.randn(r))
     m,_,k = Xhat.shape
     SSt = np.zeros((r,r,k))
     SSt = np.fft.fft(SSt,axis = -1)
     XSt = np.zeros((m,r,k))
     XSt = np.fft.fft(XSt,axis = -1)
-    for kk in range(k): # 遍历每个频率
+    for kk in range(k):
         xhatk = Xhat[:,:,kk]
         shatk = Shat[:,:,kk]
 
@@ -22,9 +20,9 @@ def tendl(Xhat,S,P): # 输入数据的FFT变换结果，另一组数据，参数
         XSt[:,:,kk] = np.dot(xhatk,np.conj(shatk).T)
 
     # optimise return x
-    bnds = tuple((0,np.infty) for i in range(len(dual_lambda))) # 边界，保证对偶变量非负
+    bnds = tuple((0,np.infty) for i in range(len(dual_lambda)))
     fun = lambda x :fobj(x,XSt,SSt,k)
-    res = sco.minimize(fun,dual_lambda,method = 'L-BFGS-B',bounds = bnds) # 求解最优化问题
+    res = sco.minimize(fun,dual_lambda,method = 'L-BFGS-B',bounds = bnds)
 
     Lambda = np.diag(res.x)
     Bhat = np.zeros((m,r,k))
@@ -35,7 +33,7 @@ def tendl(Xhat,S,P): # 输入数据的FFT变换结果，另一组数据，参数
         Bhatkt =np.dot(np.linalg.pinv(SStk+Lambda),np.conj(XStk).T)
         Bhat[:,:,kk] = np.conj(Bhatkt).T
 
-    B = np.fft.ifft(Bhat,axis = -1) # 用IFFT得到结果B，清理NANs并将数据转换为实数
+    B = np.fft.ifft(Bhat,axis = -1)
     B[np.where(np.isnan(B) == True)] = 0
     B = np.real(B)
 
@@ -46,7 +44,7 @@ def fobj(lam,XSt,SSt,k):
     r = np.size(lam)
     Lam = np.diag(lam)
     f = 0
-    for kk in range(k): # 计算每个频段的负对数似然
+    for kk in range(k): 
         XStk = XSt[:,:,kk]
         SStk = SSt[:,:,kk]
         SSt_inv = np.linalg.pinv(SStk+Lam)
